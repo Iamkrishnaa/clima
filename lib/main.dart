b75b96e931fa8fbb4f9a4b996ef9bc38a6602837
Roshan Adhikari <80669246+iamrossan@users.noreply.github.com>
2021-10-04 16:28:01 +0545

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/screens/loading_screen.dart';
 
+//this line runs the code
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
